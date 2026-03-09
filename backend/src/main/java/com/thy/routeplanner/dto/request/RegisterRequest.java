96110c230266861c21c49d771339acbb5de1c2c8
alisen <alisen4099@gmail.com>
2026-03-10 01:25:32 +0300

    Remove unused JsonIgnoreProperties annotation from RegisterRequest

--- backend/src/main/java/com/thy/routeplanner/dto/request/RegisterRequest.java
+++ backend/src/main/java/com/thy/routeplanner/dto/request/RegisterRequest.java
@@ -1,9 +1,7 @@
 package com.thy.routeplanner.dto.request;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.validation.constraints.NotBlank;
 
-@JsonIgnoreProperties(ignoreUnknown = true)
 public record RegisterRequest(
         @NotBlank String username,
         @NotBlank String password
